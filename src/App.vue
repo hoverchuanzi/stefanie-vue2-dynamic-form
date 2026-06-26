b1eb1ce7caffac3cafdf18f6f2bbb8651cf7dde3
hover <hover_chuanzi@163.com>
2026-06-26 17:26:49 +0800

    render form item

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <form-item-render></form-item-render>
+  <form-item-render :form-item-list="formList"></form-item-render>
 </div>
 </template>
 
@@ -10,6 +10,69 @@ export default {
   name: "App",
   components: {
    formItemRender,
+  },
+  data() {
+    return {
+     formList: [
+       {
+         field: "startDate",
+         label: "开始时间",
+         type: "date",
+         required: true,
+         visible: true,
+         disabled: false,
+         defaultValue: "",
+         placeholder: "",
+         span: 12
+       },
+       {
+         field: "startDate",
+         label: "结束时间",
+         type: "date",
+         required: true,
+         visible: true,
+         disabled: true,
+         defaultValue: "",
+         placeholder: "",
+         span: 12
+       },
+       {
+         field: "productName",
+         label: "产品名称",
+         type: "input",
+         required: true,
+         visible: true,
+         disabled: true,
+         defaultValue: "",
+         placeholder: "",
+         span: 12
+       },
+       {
+         field: "iisWoman",
+         label: "过滤",
+         type: "checkbox",
+         required: true,
+         visible: true,
+         disabled: false,
+         defaultValue: "",
+         placeholder: "",
+         span: 12
+       },
+       {
+         field: "accAmt",
+         label: "收款",
+         type: "number",
+         required: true,
+         visible: true,
+         disabled: false,
+         defaultValue: "",
+         placeholder: "",
+         span: 12
+       },
+
+
+     ]
+    }
   }
 }
 </script>
--- src/components/formItemRender.vue
+++ src/components/formItemRender.vue
@@ -1,12 +1,48 @@
 <template>
-  <div>
-    <a-button type="primary">
-      Primary
-    </a-button>
+  <div class='form-wrapper'>
+    <a-form :form="form" layout="inline" size="small">
+      <template v-for="(formItem, index) in formItemList">
+        <template v-if="formItem.type === 'input'">
+          <a-form-item :key="index" :label="formItem.label">
+            <a-input placeholder="Basic usage" :disabled="formItem.disabled" />
+          </a-form-item>
+        </template>
+        <template v-if="formItem.type === 'number'">
+          <a-form-item :key="index" :label="formItem.label">
+            <a-input-number :disabled="formItem.disabled"  />
+          </a-form-item>
+        </template>
+        <template v-if="formItem.type === 'date'">
+          <a-form-item :key="index" :label="formItem.label">
+            <a-date-picker :disabled="formItem.disabled"/>
+          </a-form-item>
+        </template>
+        <template v-if="formItem.type === 'checkbox'">
+          <a-form-item :key="index" :label="formItem.label">
+            <a-checkbox :disabled="formItem.disabled">
+            </a-checkbox>
+          </a-form-item>
+        </template>
+      </template>
+    </a-form>
   </div>
 </template>
 <script>
 export default {
+  props: {
+    formItemList: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
 
 }
 </script>
+
+<style>
+.form-wrapper {
+  padding: 20px 450px;
+}
+</style>
